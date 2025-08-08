ae0376e1749dec57963e462bfcd635dc320c9ea3
skymere137 <kaisergw2@gmail.com>
2025-08-07 23:44:45 -0700

    Added a function to accomodate a need for an immediate data collection and dataframe establishment. Also removed fluff and unnecessary code

--- daily.py
+++ daily.py
@@ -10,15 +10,25 @@ from contextlib import asynccontextmanager
 
 api = AsyncApiCaller()
 
+# async def main():
+    
+#     print("Hello World!")
+#     await api.get_watchlist_data(api.small_cap_tickers)
+#     await api.get_watchlist_data(api.mid_cap_tickers)
+#     get_dataframes()
+
+#     print("Goodbye World!")
+
 def get_dataframes():
     global small_cap_data
     small_cap_data = Data("small")
+    global mid_cap_data
+    mid_cap_data = Data("mid")
 
-
-small_cap_data = Data("small")
-
-schedule.every().day.at("05:00").do(lambda: api.get_watchlist_data)
-schedule.every().day.at("21:45").do(lambda: get_dataframes())
+schedule.every().day.at("05:00").do(lambda: api.get_watchlist_data(api.small_cap_tickers))
+schedule.every().day.at("05:15").do(lambda: api.get_watchlist_data(api.mid_cap_tickers))
+schedule.every().day.at("05:30").do(lambda: get_dataframes())
+schedule.every().day.at("06:00").do(lambda: get_dataframes())
 
 async def run_scheduler():
     while True:
@@ -28,6 +38,8 @@ async def run_scheduler():
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Starting app and scheduler...")
+    # await main()
+
     task = asyncio.create_task(run_scheduler())
     yield
     print("Shutting down...")
@@ -55,37 +67,3 @@ async def new_lows_json():
     new_lows = small_cap_data.find_new_lows()
     return new_lows
 
-
-
-
-
-
-
-# def run_schedule():
-#     ops = Operations()
-#     api = AsyncApiCaller()
-   
-    
-    # try:
-    #     schedule.every().day.at("01:06").do(lambda: asyncio.run(api.get_watchlist_data(api.small_cap_tickers)))
-    # except Exception as e:
-    #     print("Process could_not be run!!!")
-    #     print(e)
-    # try:
-    #     schedule.every().day.at("00:34").do(lambda: ops.get_small_cap())
-    # except Exception as e:
-    #     print("Process 2 could_not be run!!!")
-    #     print(e)
-    
-    # try:
-    #     schedule.every().day.at("00:34").do(lambda: ops.run())     
-    # except Exception as e:
-    #     print("Process 3 could_not be run!!!")
-    #     print(e)
-
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
-        
-
-# run_schedule()
